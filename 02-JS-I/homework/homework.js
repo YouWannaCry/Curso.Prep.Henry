cff363930d2ff88255871cc11f3c3c44768961df
YouWannaCry <arodrigoegea@gmail.com>
2021-10-04 20:02:30 -0300

    Tarea 1 v3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,8 +28,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  var pal = "Henry"
-  return devolverString(pal);
+  return str;
 }
 
 function suma(x, y) {
